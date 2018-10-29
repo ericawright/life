1151b181cfea1f442b870f14d8b412fc978b3c7a
Erica Wright <ericaewright@gmail.com>
2018-10-28 22:10:56 -0400

    User can customize start pattern

--- index.js
+++ index.js
@@ -1,51 +1,12 @@
 
+let board = Array.from(Array(40), _ => Array(40).fill(0));
+let animation_frame;
+let pause = false;
+
 let startGame = () => {
-
-  let createGrid = (width, height) => {
-    // Make grid with equal height and width
-    let arr = Array.from(Array(width), _ => Array(height).fill(0));
-    arr[0][1] = 1;
-    arr[1][2] = 1;
-    arr[2][0] = 1;
-    arr[2][1] = 1;
-    arr[2][2] = 1;
-    arr[4][0] = 1;
-    arr[2][5] = 1;
-    arr[7][4] = 1;
-    arr[9][6] = 1;
-    arr[0][6] = 1;
-    arr[6][5] = 1;
-    arr[8][8] = 1;
-    arr[8][6] = 1;
-    arr[0][5] = 1;
-    arr[9][6] = 1;
-    arr[1][3] = 1;
-    arr[1][2] = 1;
-    arr[2][3] = 1;
-    arr[2][2] = 1;
-    return arr;
-  }
-  let gridWidth = 10; // take user input for this in future
-  let gridHeight = 10;
-
-  let grid = createGrid(gridWidth, gridHeight);
-
-  let drawGrid = (nRow, nCol) =>  {
-    let can = document.getElementById('grid');
-    var ctx = can.getContext("2d");
-
-    ctx.clearRect(0, 0, 400, 400);
-
-      for (let j = 0; j < gridHeight; j++) {
-        for (let k = 0; k < gridWidth; k++) {
-          if (grid[j][k] == 1) {
-            ctx.fillStyle = '#FF0000';
-            ctx.fillRect(j * 30, k * 30, 30, 30);
-          }
-        }
-      }
-  }
-
+  pause = false;
+  gridWidth = 40;
+  gridHeight = 40;
   let updateGrid = () => {
     let tempGrid =  Array.from(Array(gridWidth), _ => Array(gridHeight).fill(0));
     for (let j = 0; j < gridWidth; j++) {
@@ -53,61 +14,113 @@ let startGame = () => {
         let totalCells = 0;
         //add up the total values for the surrounding cells
         if (j !=0 && k != 0) {
-          totalCells += grid[j - 1][k - 1];
+          totalCells += board[j - 1][k - 1];
         }
         if (j !=0) {
-            totalCells += grid[j - 1][k];
+            totalCells += board[j - 1][k];
         }
-        if (j !=0 && k != grid[0].length - 1) {
-            totalCells += grid[j - 1][k + 1];
+        if (j !=0 && k != board[0].length - 1) {
+            totalCells += board[j - 1][k + 1];
         }
         if (k != 0) {
-            totalCells += grid[j][k - 1];
+            totalCells += board[j][k - 1];
         }
-        if (k != grid[0].length - 1) {
-            totalCells += grid[j][k + 1];
+        if (k != board[0].length - 1) {
+            totalCells += board[j][k + 1];
         }
-        if (j != grid.length - 1 && k != 0) {
-            totalCells += grid[j + 1][k - 1];
+        if (j != board.length - 1 && k != 0) {
+            totalCells += board[j + 1][k - 1];
         }
-        if (j != grid.length - 1) {
-            totalCells += grid[j + 1][k];
+        if (j != board.length - 1) {
+            totalCells += board[j + 1][k];
         }
-        if (j != grid.length - 1 && k != grid[0].length - 1) {
-            totalCells += grid[j + 1][k + 1];
+        if (j != board.length - 1 && k != board[0].length - 1) {
+            totalCells += board[j + 1][k + 1];
         }
 
-        if (grid[j][k] == 0) {
+        if (board[j][k] == 0) {
           if (totalCells == 3) {
             tempGrid[j][k] = 1;
           }
           // Default is 0 in tempGrid, so I don't have to change it.
-        } else if (grid[j][k] == 1){
+        } else if (board[j][k] == 1){
           if (totalCells == 2 || totalCells == 3) {
             tempGrid[j][k] = 1;
           }
         }
       }
     }
-    console.log(tempGrid)
-    grid = tempGrid;
+    board = tempGrid;
   }
 
   let playGame = () => {
     drawGrid();
     updateGrid();
+    if (pause) return;
     setTimeout(function() {
-      requestAnimationFrame(playGame);
-    }, 1000);
+      animation_frame = requestAnimationFrame(playGame);
+    }, 200);
   }
   drawGrid();
-  requestAnimationFrame(playGame);
-  //
-  // document.on("keyup", function(e) {
-  //   if (e.key === "Escape") {
-  //
-  //   }
-  // });
+  animation_frame = requestAnimationFrame(playGame);
 }
 
-window.onload = startGame;
+let drawGrid = () =>  {
+  let gridWidth = 40;
+  let gridHeight = 40;
+  let can = document.getElementById('grid');
+  let blockWidth = can.width / gridWidth;
+  let blockHeight = can.height / gridHeight;
+  can.style.width = can.width;
+  can.style.height = can.height;
+
+  var ctx = can.getContext("2d");
+  ctx.clearRect(0, 0, 600, 600);
+  for (let j = 0; j < gridHeight; j++) {
+    for (let k = 0; k < gridWidth; k++) {
+      if (board[j][k] == 1) {
+        ctx.fillStyle = '#FF0000';
+        ctx.fillRect(j * blockWidth, k * blockHeight, blockWidth, blockHeight);
+      }
+    }
+  }
+}
+
+let placeNode = (e) => {
+  let ctx = document.getElementById('grid');
+  let x, y;
+  if (e.pageX || e.pageY) {
+    x = e.pageX;
+    y = e.pageY;
+  } else {
+    x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+    y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+  }
+  x -= ctx.offsetLeft;
+  y -= ctx.offsetTop;
+
+  x = Math.floor(x/15);
+  y = Math.floor(y/15);
+
+  board[x][y] = 1;
+  drawGrid();
+}
+
+let pauseGame = () => {
+  pause = true;
+}
+
+let clearBoard = () => {
+  pauseGame();
+  board = Array.from(Array(40), _ => Array(40).fill(0));
+  drawGrid();
+}
+
+let start = () => {
+  document.getElementById('grid').addEventListener('click', placeNode);
+  document.getElementById('play-button').addEventListener('click', startGame);
+  document.getElementById('pause-button').addEventListener('click', pauseGame);
+  document.getElementById('clear-button').addEventListener('click', clearBoard);
+}
+
+window.onload = start;
